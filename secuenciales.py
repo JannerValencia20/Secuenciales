59aff90f665814347936646555031810200ba9a3
janner valencia <janquival@gmail.coom>
2020-09-04 00:05:54 -0500

    secuenciales

---
+++ secuenciales.py
@@ -0,0 +1,67 @@
+
+#secuenciales
+#punto 1
+'''cap_inv=200000
+gan= cap_inv * 0.02
+print(f"Su ganancias mensuales son : {gan}")'''
+
+#punto2
+salb=877000
+v1= float(input(f"ingrese valor venta 1  "))
+v2= float(input(f"ingrese valor venta 2 "))
+v3= float(input(f"ingrese valor  venta 3 "))
+tolt_vent= v1+ v2 + v3
+comi= tolt_vent * 0.10
+tpag= salb + comi
+
+print(f"Comicion por ventas : {comi}")
+print(f"Total a ganar : {tpag}")
+
+#punto3
+'''valc=120000
+desc= valc * 0.15
+toltp= valc - desc
+print(f"Descuento : {desc}")
+print(f"Total a pagar: {toltp}") '''
+
+#punto4
+'''calf1= 40
+calf2= 33
+calf3= 50
+exaf= 30
+trabf = 40
+prom=(calf1 + calf2 + calf3)/3
+pparc=prom * 0.55
+calef = exaf * 0.30
+caltb = trabf * 0.15
+calfinal= pparc + calef + trabf
+print(prom)
+print(f"Calificacion final {calfinal}")'''
+
+#punto5
+'''numhomb = 45
+nummuj = 15
+tgrup = numhomb + nummuj
+phom= numhomb * 100/ tgrup
+pmuj= nummuj * 100 / tgrup
+
+print(f"Promedio hombres {phom}")
+print(f"promedio mujeres {pmuj}")'''
+
+#punto6
+
+'''fnac=1999
+fact=2020
+edad= fact - fnac
+print(f"atualmente tiene {edad} años de edad")'''
+
+
+
+
+
+
+
+
+
+
+
